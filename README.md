f58725bc42ac258e20f858f4b5b7890102973ba6
Douglas Lopes <dl.aguiar@yahoo.com.br>
2025-02-19 18:52:42 -0300

    Add instructions to use the library in filament php projects

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ Set your Gemini API key in your .env file:
 GEMINI_API_KEY=your_api_key_here
 ```
 
-## Usage
+## Add to layout
 
 1. Add the required scripts to your layout file (`app.blade.php` or similar). Make sure to add them in this exact order:
 
@@ -51,9 +51,32 @@ php artisan vendor:publish --tag=ai-chat-views
 
 The views will be published to `resources/views/vendor/ai-chat/components/`.
 
+## If you want to add ai-chat to filament php
+
+Add this following code to AdminPanelProvider.php
+
+```php
+// Makes tailwind classes being compiled
+->viteTheme('resources/css/app.css')
+// Add assets
+->assets([
+    Js::make('ai-chat-js', asset('vendor/ai-chat/js/ai-chat.js')),
+])
+// Add blade components
+->renderHook(
+    PanelsRenderHook::BODY_END,
+    function () {
+        if (auth()->check()) {
+            return view('vendor.ai-chat.components.ai-floating-chat');
+        }
+    }
+)
+```
+
 ## Todo
 
 - [x] Implement GeminiProvider
+- [ ] Add the ability to execute constructive queries (create, update)
 - [ ] Implement OpenAiProvider
 - [ ] Implement AnthropicProvider
 
---
+++ vendor/composer/installed.php
@@ -0,0 +1,98 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dougkusanagi/laravel-ai-chat',
+        'pretty_version' => '0.1.17',
+        'version' => '0.1.17.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dougkusanagi/laravel-ai-chat' => array(
+            'pretty_version' => '0.1.17',
+            'version' => '0.1.17.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'google-gemini-php/client' => array(
+            'pretty_version' => '1.0.15',
+            'version' => '1.0.15.0',
+            'reference' => '6dca9a5e6a6de39b230fe4593a8d041efd02e34e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../google-gemini-php/client',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'illuminate/contracts' => array(
+            'pretty_version' => 'v11.43.1',
+            'version' => '11.43.1.0',
+            'reference' => 'b350a3cd8450846325cb49e1cbc1293598b18898',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../illuminate/contracts',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'php-http/async-client-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '*',
+            ),
+        ),
+        'php-http/client-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '*',
+            ),
+        ),
+        'php-http/discovery' => array(
+            'pretty_version' => '1.20.0',
+            'version' => '1.20.0.0',
+            'reference' => '82fe4c73ef3363caed49ff8dd1539ba06044910d',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../php-http/discovery',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/container' => array(
+            'pretty_version' => '2.0.2',
+            'version' => '2.0.2.0',
+            'reference' => 'c71ecc56dfe541dbd90c5360474fbc405f8d5963',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/container',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/http-client-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '*',
+            ),
+        ),
+        'psr/http-factory-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '*',
+            ),
+        ),
+        'psr/http-message-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '*',
+            ),
+        ),
+        'psr/simple-cache' => array(
+            'pretty_version' => '3.0.0',
+            'version' => '3.0.0.0',
+            'reference' => '764e0b3939f5ca87cb904f570ef9be2d78a07865',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/simple-cache',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
